763482eed8310c5e68a11354e28eb1275b301185
Daniel Bruch <daniel.bruch@hub2b.com.br>
2021-08-08 19:18:12 -0300

    Incial

---
+++ GerenciadorSeries.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GerenciadorSeries.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
